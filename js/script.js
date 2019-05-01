f1918a534eaf8475a4006eb9e5359f2d35ce8c49
MichalAW <michal.wanta@gmail.com>
2019-05-01 22:27:39 +0200

    fix

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var GalleryItem = React.createClass({
+    render: function() {
+        return React.createElement('h2', {}, 'Pierwszy komponent');
+    }
+});
+
+var GalleryItem = React.createClass({
+    propTypes: {
+        image: React.PropTypes.object.isRequired,
+    },
+
+    render: function() {
+        return (
+            React.createElement('div', {},
+            React.createElement('h2', {}, this.props.image.name),
+            React.createElement('img', {src: this.props.image.src})
+        )
+    )},
+});
+
+var image = {
+    name: 'Kotek',
+    src: 'http://imgur.com/n8OYCzR.png'
+};
+
+var element = React.createElement(GalleryItem, {image: image})
+
+var element = React.createElement(GalleryItem);
+ReactDOM.render(element, document.getElementById('app'));
